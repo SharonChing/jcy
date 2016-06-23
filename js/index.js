084fef146220cc318f148d7fec63efdf0ea9aaa7
bonedeng <bonedeng@bonedengs-MacBook-Pro.local>
2016-06-23 16:39:57 +0800

    dengpeng: 添加幻灯片js

--- js/index.js
+++ js/index.js
@@ -1,29 +1,39 @@
 $(document).ready(function () {
-  var perWidth = $(".carousel-box ul li").width();
-  var imagesLength = $(".carousel-box ul li").length;
-  var widthOfUL = imagesLength*perWidth;
+  //先获取ul下面有多少个li
+  var numberOfLis = $(".carousel-box ul li").length;
+  var widthOfLi = $('.carousel-box ul li').width();
+
+  var widthOfUL = numberOfLis * widthOfLi;
   $(".carousel-box ul").width(widthOfUL);
 
-  $('.turn-arrow a:last').click(function (e) {
-    e.preventDefault();
-    var left = perWidth;
-    $('.carousel-box ul').animate({'right': left+'px'}, function () {
-      $('.carousel-box ul').append($('.carousel-box ul li:first').clone());
-      $('.carousel-box ul li:first').remove();
-      $('.carousel-box ul').css({
-        "right": "0px"
+
+  $(".turn-arrow a:first").click(function (e) {
+    e.preventDefault(); //阻止掉默认行为
+    $(".carousel-box ul").animate({
+      right: widthOfLi + "px" //向右边偏移一平
+    }, function () {
+      var firstLi = $(".carousel-box ul li:first").clone(); //拷贝一份第一个节点
+      $(".carousel-box ul").append(firstLi); //将第一个节点添加到最后
+      $(".carousel-box ul li:first").remove(); //吧第一个节点移除，让第二个节点作为第一个节点
+      $(".carousel-box ul").css({ //还原偏移量，下次执行的时候right有事从0-一瓶的距离
+        right: "0px"  //只有属性值有差异比如从0变为100这些变化才会产生动画
       });
     });
   });
-  $('.turn-arrow a:first').click(function (e) {
+
+  $(".turn-arrow a:last").click(function (e) {
     e.preventDefault();
-    var right = perWidth;
-    $('.carousel-box ul').prepend($('.carousel-box ul li:last').clone());
-    $('.carousel-box ul').css({
-      "right": "1216px"
+    var lastLi = $(".carousel-box ul li:last").clone(); //拷贝最后一个
+    $(".carousel-box ul").prepend(lastLi); //放在最前面其实是-1的一个
+    $(".carousel-box ul").css({ //然他默认偏移到1的位置
+      right: widthOfLi + "px"
     });
-    $('.carousel-box ul').animate({'right': '0px'}, function() {
-      $('.carousel-box ul li:last').remove();
+    $(".carousel-box ul").animate({ //然他动画偏移到-1的位置
+      right: "0px"
+    }, function () {
+      $(".carousel-box ul li:last").remove(); //删掉拷贝出来的原来的最后那个
     });
   });
+
+
 });
