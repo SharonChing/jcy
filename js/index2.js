47acf58e944f1fce1a787ac75a24501ddb0c4291
bonedeng <bonedeng@bonedengs-MacBook-Pro.local>
2016-06-25 14:41:29 +0800

    [dengpeng]: 添加幻灯片动画

--- js/index2.js
+++ js/index2.js
@@ -1,16 +1,34 @@
 $(document).ready(function () {
+  var liElemet = $(".carousel-box ul li");
+  var ulElemet = $(".carousel-box ul");
+  var firstLi = $(".carousel-box ul li:first");
+  var lastLi = $(".carousel-box ul li:last");
+  var widthOfLI = liElemet.width();
+  var lengthOfLI = liElemet.length;
+  var widthOfUl = widthOfLI * lengthOfLI;
+  ulElemet.width(widthOfUl);
 
-  function (str, 2s) {
-
-  }
-
-  $('.designer-carousel-scroll').click(function (e) {
-    e.preventDefault();
-    console.log('fff');
-    $('.designer-carousel ul li:first').animate({
-      transform: "translate(0, 1000px)"
-    }, function () {
-      //将当前第一个切换到最后一个
+  $(".turn-arrow a:last").click(function(event) {
+    event.preventDefault();
+    var property = {
+      right: widthOfLI + "px"
+    };
+    ulElemet.animate(property, function () {
+      ulElemet.css("right", "0px");
+      ulElemet.append(firstLi.clone());
+      firstLi.remove();
     });
   });
+
+  $(".turn-arrow a:first").click(function (event) {
+    event.preventDefault();
+    ulElemet.prepend(lastLi.clone());
+    lastLi.remove();
+    ulElemet.css('right', widthOfLI + "px");
+    var property = {
+      right: "0px"
+    };
+    ulElemet.animate(property);
+  });
+
 });
