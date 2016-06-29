4501590d51ccb83e47e3eecf98d633218c30d02b
SharonChing <381493793@qq.com>
2016-06-29 11:02:28 +0800

    bug

--- js/index.js
+++ js/index.js
@@ -8,13 +8,13 @@ $(document).ready(function () {
 
 
   $(".turn-arrow a:last").click(function (e) {
-    e.preventDefault(); //阻止掉默认行为
+    e.preventDefault(); //阻止默认行为
     $(".carousel-box ul").animate({
       right: widthOfLi + "px" //向右边偏移一平
     }, function () {
       var firstLi = $(".carousel-box ul li:first").clone(); //拷贝一份第一个节点
       $(".carousel-box ul").append(firstLi); //将第一个节点添加到最后
-      $(".carousel-box ul li:first").remove(); //吧第一个节点移除，让第二个节点作为第一个节点
+      $(".carousel-box ul li:first").remove(); //把第一个节点移除，让第二个节点作为第一个节点
       $(".carousel-box ul").css({ //还原偏移量，下次执行的时候right有事从0-一瓶的距离
         right: "0px"  //只有属性值有差异比如从0变为100这些变化才会产生动画
       });
@@ -25,10 +25,10 @@ $(document).ready(function () {
     e.preventDefault();
     var lastLi = $(".carousel-box ul li:last").clone(); //拷贝最后一个
     $(".carousel-box ul").prepend(lastLi); //放在最前面其实是-1的一个
-    $(".carousel-box ul").css({ //然他默认偏移到1的位置
+    $(".carousel-box ul").css({ //让他默认偏移到1的位置
       right: widthOfLi + "px"
     });
-    $(".carousel-box ul").animate({ //然他动画偏移到-1的位置
+    $(".carousel-box ul").animate({ //让他动画偏移到-1的位置
       right: "0px"
     }, function () {
       $(".carousel-box ul li:last").remove(); //删掉拷贝出来的原来的最后那个
